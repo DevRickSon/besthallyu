0eb19e94241df96b6fc76aff77874fcd86c27f5a
DevRickSon <jhson@encar.com>
2017-06-27 11:45:51 +0900

    client 입력단 추가

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
 
+import mongoose from 'mongoose';
+import Board from './models/Board';
+
 import morgan from 'morgan';
 
 const app = express();
@@ -15,6 +18,13 @@ app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log('Connected to mongodb server');
+});
+mongoose.connect('mongodb://jhson:wjdgh031952@ds139242.mlab.com:39242/vsdb');
 
 app.use('/', express.static(path.join(__dirname, '../static')));
 
@@ -33,14 +43,45 @@ app.get('/join', (req, res) => {
 });
 
 app.post('/registerVideo', (req, res) => {
-    let { vurl, vfile, vname, vdesc} = req.body;
+    let { vurl, vfile, vname, vorigin, ufirst, ulast, unation, usns1, usns2, uemail, uvisit, upassport, uvisa, ucancel } = req.body;
     console.log('url: ', vurl);
     console.log('file: ', vfile);
-    console.log('name: ', vname);
-    console.log('desc: ', vdesc);
+    console.log('vname: ', vname);
+    console.log('origin: ', vorigin);
+    console.log('name: ', ufirst, ' ', ulast);
+    console.log('unation: ', unation);
+    console.log('usns1: ', usns1);
+    console.log('usns2: ', usns2);
+    console.log('uemail: ', uemail);
+    console.log('uvisit: ', uvisit);
+    console.log('upassport: ', upassport);
+    console.log('uvisa: ', uvisa);
+    console.log('ucancel: ', ucancel);
     //유효성 체크
     //db insert
     //성공, 에러 페이지
+
+    let uname = ufirst + ' ' + ulast;
+    let usns = usns1;
+    if(typeof usns2 !== 'undefined') usns += ', ' + usns2
+
+    const respond = () => {
+        res.json({
+            success: true
+        });
+    };
+
+    const onError = (err) => {
+        res.status(409).json({
+            success: false,
+            error: err,
+            message: err.message
+        });
+    }
+
+    Board.create(vurl, vfile, vname, vorigin, uname, unation, usns, uemail, uvisit, upassport, uvisa, ucancel)
+         .then(respond)
+         .catch(onError);
 });
 
 //에러 핸들러 등록
---
+++ src/models/Board.js
@@ -0,0 +1,96 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+import autoIncrement from 'mongoose-auto-increment';
+autoIncrement.initialize(mongoose.connection);
+
+const Board = new Schema({
+    vurl: String,
+    vfile: String,
+    vname: String,
+    vorigin: String,
+    uname: String,
+    unation: String,
+    usns: String,
+    uemail: String,
+    uvisit: String,
+    upassport: String,
+    uvisa: String,
+    ucancel: String,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+Board.statics.create = function(vurl, vfile, vname, vorigin, uname, unation, usns, uemail, uvisit, upassport, uvisa, ucancel){
+    const board = new this({
+        vurl,
+        vfile,
+        vname,
+        vorigin,
+        uname,
+        unation,
+        usns,
+        uemail,
+        uvisit,
+        upassport,
+        uvisa,
+        ucancel
+    });
+
+    return board.save();
+};
+
+Board.statics.getTotal = function(query){
+    return this.count(query).exec();
+};
+
+Board.statics.getList = function(query, pagenation){
+    return this.find(query)
+               .sort('-num')
+               .skip(pagenation.startBoard)
+               .limit(pagenation.size)
+               .exec();
+};
+
+Board.statics.getPagenation = function(page, total){
+    const size = 10;
+    const pageSize = 5;
+
+    let pagenation = {};
+    let startBoard = (page - 1) * size;
+    let totalPage = Math.ceil(total / size);
+    let startPage = (Math.floor((page - 1) / pageSize) * pageSize) + 1;
+    let endPage = startPage + (pageSize - 1);
+    if(endPage > totalPage) endPage = totalPage;
+
+    let pages = [];
+    for(var i = startPage; i <= endPage; i++){
+        pages.push(i);
+    }
+
+    let prevPage = (page < pageSize) ? -1 : startPage - pageSize;
+    let nextPage = (endPage === totalPage) ? -1 : endPage + 1;
+
+    pagenation.current = page;
+    pagenation.startBoard = startBoard;
+    pagenation.startPage = startPage;
+    pagenation.endPage = endPage;
+    pagenation.pages = pages;
+    pagenation.prevPage = prevPage;
+    pagenation.nextPage = nextPage;
+    pagenation.size = size;
+    pagenation.pageSize = pageSize;
+
+    return pagenation;
+};
+
+Board.plugin(autoIncrement.plugin, {
+    'model': 'Board',
+    'field': 'num',
+    'startAt': 1,
+    'incrementBy': 1
+});
+
+export default mongoose.model('Board', Board);
