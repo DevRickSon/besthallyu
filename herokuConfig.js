de00961d1410184e037b8289b5de2ceccedf90f1
DevRickSon <dev.rickson@gmail.com>
2017-07-20 14:57:56 +0900

    Delete herokuConfig.js

--- herokuConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: 'mongodb://rickSon:wjdgh031952!@ds037622.mlab.com:37622/heroku_gmdzqqw3',
-    secret: 'sEcReTkeyOfVistseOuL',
-    sid: 'KeyOfvsSeoul',
-    accessKeyId: 'AKIAIK4DIV74OJLPSD2A',
-    secretAccessKey: '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',
-    region: 'ap-northeast-2',
-    bucket: 'besthallyu'
-};
